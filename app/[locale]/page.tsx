5502ed022dd00afc99d7120daeeda349a97f7050
Andrii Roizinn <andreyroizin14@gmail.com>
2025-11-13 19:54:19 +0100

    redirect workaround

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,12 +1,23 @@
+import Link from "next/link";
 import { Typography } from "@mui/material";
 
-export default function Home() {
+type HomeProps = {
+    params: {
+        locale: string;
+    };
+};
+
+export default function Home({ params: { locale } }: HomeProps) {
+    const loginHref = `/${locale}/auth/login`;
+
     return (
         <div className="grid  items-center justify-items-center p-8 pb-20 gap-16">
             <div>
-                <Typography variant={"h4"} gutterBottom>
-                    Welcome to VervoerManager
-                </Typography>
+                <Link href={loginHref} style={{ textDecoration: "none", color: "inherit" }}>
+                    <Typography variant={"h4"} gutterBottom sx={{ cursor: "pointer" }}>
+                        Welcome to VervoerManager
+                    </Typography>
+                </Link>
             </div>
         </div>
     );
